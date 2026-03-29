2f91f90bec24ec36e67d318c2dd379a0e7137a1f
Agustina Sosa <agustinaayelensosa2@gmail.com>
2026-03-29 00:20:01 -0300

    maquetado del home

--- src/view/auth/view/landing/homeView/Components/HeroText.tsx
+++ src/view/auth/view/landing/homeView/Components/HeroText.tsx
@@ -1,7 +1,8 @@
-import { CiLogin } from "react-icons/ci";
 import { IoIosArrowForward } from "react-icons/io";
 import Button from "../../../../../components/CustomeButton";
 import HeroImg from "./HeroImg";
+import delCesar from "../../../../../../../public/textDelCesar.png";
+import { FaWhatsapp } from "react-icons/fa6";
 
 interface Props {
   handleDrawer: () => void;
@@ -10,10 +11,8 @@ export default function HeroText({ handleDrawer }: Props) {
   return (
     <div className="px-4  lg:px-0 lg:w-1/2 h-full  flex items-center md:justify-center lg:justify-end">
       <div className=" md:w-3/4 2xl:w-3/5 space-y-6 md:space-y-8">
-        <div className=" md:space-y-3 text-center lg:text-start">
-        <h1 className="text-lg md:text-4xl lg:text-6xl tracking-wide font-bold italic font-cursive text-default-text">
-  Lo del Cesar
-</h1>
+        <div className=" flex flex-col items-center space-y-3 text-center lg:text-start">
+          <img src={delCesar} alt="icono" className="w-40" />
           <h2 className="text-xl md:text-3xl lg:text-2xl font-semibold text-colorTres">
             Comidas Caceras
           </h2>
@@ -22,23 +21,30 @@ export default function HeroText({ handleDrawer }: Props) {
           <HeroImg />
         </div>
         <p className="text-sm md:text-base lg:text-base 2xl:text-lg tracking-wider pr-3">
-        <span className="font-semibold">¡Anímate a probar lo mejor de nuestra cocina!</span>   <br />Disfruta de un menú variado con pizzas, empanadas, sándwiches de milanesa y hamburguesas, además de nuestros platos especiales del día.
-       
+          <span className="font-semibold">
+            ¡Anímate a probar lo mejor de nuestra cocina!
+          </span>
+          <br />
+          Disfruta de un menú variado con pizzas, empanadas, sándwiches de
+          milanesa y hamburguesas, además de nuestros platos especiales del día.
         </p>
-        <div className="w-full flex justify-center md:justify-start gap-8">
+        <div className="w-full flex justify-center md:justify-start gap-3">
           <Button
             onClick={handleDrawer}
-            text="Empezar Ahora"
+            text="Ver Comidas"
             claseButton="primary"
-            Icon={CiLogin}
-            positionIcon="right"
-          />
-          <Button
-            text="Contáctanos"
-            claseButton="secondary"
-            border
             Icon={IoIosArrowForward}
             positionIcon="right"
+            color="bg-colorTres"
+          />
+          <Button
+            text="Consultar Plato del Día"
+            claseButton="secondary"
+            border
+            Icon={FaWhatsapp}
+            positionIcon="right"
+            color="text-colorTres"
+            borderColor="border-colorTres"
           />
         </div>
       </div>
--- src/view/auth/view/landing/homeView/view/Home.tsx
+++ src/view/auth/view/landing/homeView/view/Home.tsx
@@ -9,8 +9,8 @@ interface Props {
 export default function Home({ handleDrawer }: Props) {
   return (
     <Element name="inicio">
-<section className="bg-black/50 bg-white bg-cover bg-blend-darken lg:h-[630px] 2xl:h-[800px] w-full flex flex-col lg:flex-row items-center py-10 lg:py-0">        <HeroText handleDrawer={handleDrawer} />
-
+      <section className="bg-black/50 bg-white bg-cover bg-blend-darken lg:h-[630px] 2xl:h-[800px] w-full flex flex-col lg:flex-row items-center py-10 lg:py-0">
+        <HeroText handleDrawer={handleDrawer} />
         <div className="hidden lg:block">
           <HeroImg />
         </div>
